6352ffc2dd81f13758d8a38927f208c573ee937c
Regina Tavabilova <rtavabil@c2r3s10.42london.com>
2024-08-14 17:06:07 +0100

    ex02

--- CPP0/ex01/includes/Contact.hpp
+++ CPP0/ex01/includes/Contact.hpp
@@ -23,11 +23,13 @@ public:
 	void		set_darkest_secret(std::string darkest_secret);
 
 private:
+
 	std::string	first_name;
 	std::string	last_name;
 	std::string	nickname;
 	std::string	phone_number;
 	std::string	darkest_secret;
+
 };
 
 #endif
