ba641af1f6d13932ee7c777b568ce14a56a05f52
Regina Tavabilova <rtavabil@c2r3s10.42london.com>
2024-08-28 09:18:41 +0100

    cpp1/ex05

---
+++ CPP1/ex05/src/Harl.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "../includes/Harl.hpp"
+
+Harl::Harl() {
+	std::cout << "Harl constructor called" << std::endl;
+}
+
+Harl::~Harl() {
+	std::cout << "Harl destructor called" << std::endl;
+}
+
+void	Harl::debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void) {
+	std::cout << 
+	"I cannot believe adding extra bacon costs more money. \
+You didn't put enough bacon in my burger! If you did, I \
+wouldn't be asking for more!" 
+		<< std::endl;
+}
+
+void	Harl::warning(void) {
+	std::cout << 
+	"I think I deserve to have some extra bacon for free. \
+I've been coming for years whereas you started working \
+here since last month." 
+		<< std::endl;
+}
+
+void	Harl::error(void) {
+	std::cout << 
+	"This is unacceptable! I want to speak to the manager now." 
+		<< std::endl;
+}
+
+void	Harl::complain(std::string level) {
+
+	void (Harl::*complaints[4])(void);
+	complaints[0] = &Harl::debug;
+	complaints[1] = &Harl::info;
+	complaints[2] = &Harl::warning;
+	complaints[3] = &Harl::error;
+
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level) {
+			(this->*complaints[i])();
+			return ;
+		}
+	}
+}
